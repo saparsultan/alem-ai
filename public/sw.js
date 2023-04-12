cc36c2a378b658f1b1e24d0ee5066896bbb1eb75
Syzdykov Sultan <sultansyzdyc@gmail.com>
2023-04-12 14:49:29 +0600

    add: add service-worker file

---
+++ public/sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'my-pwa-cache-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // Если ресурс найден в кэше, возвращаем его
+        if (response) {
+          return response;
+        }
+
+        // Если ресурс не найден в кэше, запрашиваем его с сервера
+        return fetch(event.request)
+          .then(response => {
+            // Если ответ от сервера успешный, клонируем его и сохраняем в кэше перед возвратом
+            if (response && response.status === 200 && response.type === 'basic') {
+              const responseToCache = response.clone();
+              caches.open(CACHE_NAME)
+                .then(cache => {
+                  cache.put(event.request, responseToCache);
+                });
+            }
+            return response;
+          });
+      })
+  );
+});
