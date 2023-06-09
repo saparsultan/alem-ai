bfc8680d14b61f0afaceb13227d61d7622dcef04
Syzdykov Sultan <sultansyzdyc@gmail.com>
2023-06-09 16:51:25 +0600

    add: add preview modal and refactor style app

--- src/components/BaseImageCreate.jsx
+++ src/components/BaseImageCreate.jsx
@@ -2,8 +2,9 @@ import React, {useState, useEffect} from "react";
 import {HashLoader} from 'react-spinners';
 import CheckboxSize from "./CheckboxSize";
 import SettingStyles from "./SettingStyles";
-import emptyImg from "../assets/images/gradient.jpg"
+import ModalEdit from "./ModalEdit";
 import downloadSvg from "../assets/images/download.svg"
+import expandSvg from "../assets/images/expand.svg"
 import size1 from "../assets/images/9x16.svg"
 import size2 from "../assets/images/1x1.svg"
 import size3 from "../assets/images/16x9.svg"
@@ -16,9 +17,8 @@ const override = {
     borderColor: "red",
 };
 
+
 const BaseImage = () => {
-    const HTTP_GENERATE = "https://api.amadao.network/api/dream/generate";
-    const HTTP_STYLES = "https://api.amadao.network/api/dream/styles";
     const [loading, setLoading] = useState(false);
     const [value, setValue] = useState(27);
     const [size, setSize] = useState("9x16");
@@ -26,26 +26,27 @@ const BaseImage = () => {
     const [stylesData, setStylesData] = useState([]);
     const [imageGenerate, setImageGenerate] = useState(null);
 
+    const [open, setOpen] = useState(false);
+    const [isPreview, setIsPreview] = useState(false);
+
     useEffect(() => {
         fetchStyles().then(r => {
             setStylesData(r)
         }).catch(e => console.error(e))
     }, [])
 
-    console.log({imageGenerate})
-
     async function fetchStyles() {
-        const res = await fetch(`${HTTP_STYLES}`)
+        const res = await fetch(process.env.REACT_APP_HTTP_STYLES)
         return await res.json()
     }
 
     const handleSubmit = (e) => {
         e.preventDefault()
         setLoading(true)
-        fetch(`${HTTP_GENERATE}`, {
+        fetch(process.env.REACT_APP_HTTP_GENERATE, {
             method: "POST",
             headers: {},
-            body: JSON.stringify({description: prompt, style: value, ratio: size})
+            body: JSON.stringify({style: value, description: prompt, ratio: size})
         }).then((res) => res.json()).then((resJson) => {
             setImageGenerate(resJson)
         }).catch((e) => {
@@ -73,77 +74,105 @@ const BaseImage = () => {
         }
     };
 
+    const handleOpenModal = (e) => {
+        e.preventDefault()
+        setIsPreview(false)
+        setOpen(!open)
+    }
+
+    const handleOpenModalPreview = (e) => {
+        e.preventDefault()
+        setIsPreview(!isPreview)
+        setOpen(!open)
+    }
+
+    const isNotAllowed = loading ? {cursor: "not-allowed"} : {}
+
     return (
-        <div className="base base--create-image">
-            <div className="create-image">
-                {/*<h2>Создать произведений искусства</h2>*/}
-                <div className="artwork-grid">
-                    <div className="artwork-setting">
-                        <div className="artwork-item artwork-setting__size">
-                            <h3 className="artwork-title">Размер произведения</h3>
-                            <div className="setting-size">
-                                <CheckboxSize value="9x16" onChange={handleChange} checked={size}>
-                                    <img src={size1} alt="9x16"/>
-                                </CheckboxSize>
-                                <CheckboxSize value="1x1" onChange={handleChange} checked={size}>
-                                    <img src={size2} alt="1x1"/>
-                                </CheckboxSize>
-                                <CheckboxSize value="16x9" onChange={handleChange} checked={size}>
-                                    <img src={size3} alt="16x9"/>
-                                </CheckboxSize>
-                                <CheckboxSize value="4x3" onChange={handleChange} checked={size}>
-                                    <img src={size4} alt="4x3"/>
-                                </CheckboxSize>
-                                <CheckboxSize value="3x4" onChange={handleChange} checked={size}>
-                                    <img src={size5} alt="3x4"/>
-                                </CheckboxSize>
+        <>
+            <div className="base base--create-image">
+                <div className="create-image">
+                    {/*<h2>Создать произведений искусства</h2>*/}
+                    <div className="artwork-grid">
+                        <div className="artwork-setting" style={isNotAllowed}>
+                            <div className="artwork-item artwork-setting__size">
+                                <h3 className="artwork-title">Размер произведения</h3>
+                                <div className="setting-size">
+                                    <CheckboxSize value="9x16" onChange={handleChange} checked={size} loading={loading}>
+                                        <img src={size1} alt="9x16"/>
+                                    </CheckboxSize>
+                                    <CheckboxSize value="1x1" onChange={handleChange} checked={size} loading={loading}>
+                                        <img src={size2} alt="1x1"/>
+                                    </CheckboxSize>
+                                    <CheckboxSize value="16x9" onChange={handleChange} checked={size} loading={loading}>
+                                        <img src={size3} alt="16x9"/>
+                                    </CheckboxSize>
+                                    <CheckboxSize value="4x3" onChange={handleChange} checked={size} loading={loading}>
+                                        <img src={size4} alt="4x3"/>
+                                    </CheckboxSize>
+                                    <CheckboxSize value="3x4" onChange={handleChange} checked={size} loading={loading}>
+                                        <img src={size5} alt="3x4"/>
+                                    </CheckboxSize>
+                                </div>
                             </div>
-                        </div>
-                        <div className="artwork-item artwork-setting__prompt setting-prompt">
-                            <div className="artwork-title setting-prompt__caption">Введите подсказку</div>
-                            <input type="text" className="setting-prompt__input" placeholder="Напишите что-нибудь"
-                                   value={prompt} onChange={handlePrompt}/>
-                        </div>
-                        <div className="artwork-title setting-prompt__caption">Выберите стиль произведения</div>
-                        <div className="artwork-item artwork-setting__styles setting-styles">
-                            <SettingStyles data={stylesData} value={value} handleChangeStyles={handleChangeStyles}/>
-                        </div>
-                        <button className="setting-submit" onClick={handleSubmit} disabled={prompt === ""}>Создать
-                        </button>
-                    </div>
-                    <div className="artwork-result">
-                        <div className="artwork-result__wrapper">
-                            <h3 className="artwork-title artwork-result__title">Результат произведения</h3>
-                            <div className="artwork-result__image">
-                                {
-                                    loading && <div className="artwork-result__loader result-loader">
-                                        <HashLoader
-                                            color="#a47af9"
-                                            cssOverride={override}
-                                            size={50}
-                                            aria-label="Loading Spinner"
-                                            data-testid="loader"
-                                        />
-                                        <div className="result-loader__title">Создание...</div>
-                                    </div>
-                                }
-                                {
-                                    imageGenerate ? <img src={imageGenerate?.result} alt={imageGenerate?.model?.name}/> :
-                                        <img src={emptyImg} alt="empty img"/>
-                                }
+                            <div className="artwork-item artwork-setting__prompt setting-prompt">
+                                <div className="artwork-title setting-prompt__caption">Введите подсказку</div>
+                                <input type="text" className="setting-prompt__input" placeholder="Напишите что-нибудь"
+                                       value={prompt} onChange={handlePrompt} style={isNotAllowed} disabled={loading} />
                             </div>
-                            <div className="artwork-result__footer">
-                                <button onClick={(e) => handleDownload(imageGenerate?.result, e)}
-                                        className="artwork-result__download" disabled={imageGenerate?.result}>
-                                    <img src={downloadSvg} alt="Download image"/>
+                            <div className="artwork-title setting-prompt__caption">Выберите стиль произведения</div>
+                            <div className="artwork-item artwork-setting__styles setting-styles">
+                                <SettingStyles data={stylesData} value={value} handleChangeStyles={handleChangeStyles} loading={loading}/>
+                            </div>
+                            <button className="setting-submit" onClick={handleSubmit} disabled={prompt === "" || loading}>Создать
+                            </button>
+                        </div>
+                        <div className="artwork-result">
+                            <div className="artwork-result__header">
+                                <h3 className="artwork-title artwork-result__title">Результат произведения</h3>
+                                <button className="artwork-result__download btn-action"
+                                        onClick={handleOpenModalPreview} disabled={!imageGenerate?.result || loading}>
+                                    <img src={expandSvg} alt="Expand"/>
                                 </button>
                             </div>
-                        </div>
+                            <div className="artwork-result__wrapper">
+                                <div className="artwork-result__image">
+                                    {
+                                        loading && <div className="artwork-result__loader result-loader">
+                                            <HashLoader
+                                                color="#e2b0ff"
+                                                cssOverride={override}
+                                                size={42}
+                                                aria-label="Loading Spinner"
+                                                data-testid="loader"
+                                            />
+                                            <div className="result-loader__title">Создание...</div>
+                                        </div>
+                                    }
+                                    {
+                                        (imageGenerate && !loading) && <img src={imageGenerate?.result} alt={imageGenerate?.model?.name}/>
+                                    }
+                                </div>
+                                <div className="artwork-result__footer">
+                                    <button className="artwork-result__edit btn-action"
+                                             onClick={handleOpenModal} title="В разработке..." disabled>Изменить
+                                    </button>
+                                    <button onClick={(e) => handleDownload(imageGenerate?.result, e)}
+                                            className="artwork-result__download btn-action"
+                                            disabled={!imageGenerate?.result || loading}>
+                                        <img src={downloadSvg} alt="Download"/>
+                                    </button>
+                                </div>
+                            </div>
 
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            {
+                open && <ModalEdit open={open} isPreview={isPreview} handleOpen={handleOpenModal} data={imageGenerate} description={prompt} style={value} ratio={size} idImg={imageGenerate?.id} />
+            }
+        </>
     );
 };
 
